3c5193214fc33ae889ab09b5bfb8e148bda3eec9
osama-rgp <osamagh5945@gamil.com>
2023-06-05 21:21:15 -0400

    upload new version

--- lib/core/common/model/home.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter/material.dart';
-class Home {
-  final String id;
-  final String title;
-  final String images;
-
-  const Home({required this.id, required this.title, required this.images});
-}
